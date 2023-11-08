70259ba3ada6bf5b70a468a04743c3d6c65ffcd6
Carlos Lozada <ce.lozada@icloud.com>
2023-11-08 12:58:53 -0600

    Retrofit Dependencies for the use of API responses in XML

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.squareup.picasso:picasso:2.71828")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0") // Retrofit JSON converter
+    implementation ("com.squareup.retrofit2:converter-simplexml:2.9.0") // Retrofit Simple XML converter
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
