f98d7cbef102af95257ebfb8b7e1d4836ae3b13a
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-11-10 12:07:12 -0600

    remove unnecessary libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,11 +44,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("com.squareup.picasso:picasso:2.71828")
+
+    // API
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0") // Retrofit JSON converter
-    implementation ("com.squareup.retrofit2:converter-simplexml:2.9.0") // Retrofit Simple XML converter
+    implementation ("com.squareup.retrofit2:converter-simplexml:2.9.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.6")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
