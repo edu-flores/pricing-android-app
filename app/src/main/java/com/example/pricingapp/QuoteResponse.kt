685f0d95fd73c6190d3cd6993b95d7f0d66c9164
Carlos Lozada <ce.lozada@icloud.com>
2023-11-11 16:37:19 -0600

    Added data classes to receive the terminal's information from the API.

--- app/src/main/java/com/example/pricingapp/QuoteResponse.kt
+++ app/src/main/java/com/example/pricingapp/QuoteResponse.kt
@@ -12,8 +12,58 @@ data class Error(
     var errorMessage: String? = null
 )
 
+@Root(name = "ORIGTERMINFO", strict = false)
+data class OriginTerminalInfo(
+    @field:Element(name = "ORIGTERMADDRESS", required = false)
+    var originTerminalAddress: String? = null,
+
+    @field:Element(name = "ORIGTERMCITY", required = false)
+    var origingTerminalCity: String? = null,
+
+    @field:Element(name = "ORIGTERMSTATE", required = false)
+    var origingTerminalState: String? = null,
+
+    @field:Element(name = "ORIGTERMZIP", required = false)
+    var originTerminalZip: String? = null,
+
+    @field:Element(name = "ORIGTERMPHONE", required = false)
+    var originTerminalPhone: String? = null,
+
+    @field:Element(name = "TYPE", required = false)
+    var originTerminalType: String? = null
+)
+
+@Root(name = "DESTTERMINFO", strict = false)
+data class DestinationTerminalInfo(
+    @field:Element(name = "DESTTERMADDRESS", required = false)
+    var destinationTerminalAddress: String? = null,
+
+    @field:Element(name = "DESTTERMCITY", required = false)
+    var destinationTerminalCity: String? = null,
+
+    @field:Element(name = "DESTTERMSTATE", required = false)
+    var destinationTerminalState: String? = null,
+
+    @field:Element(name = "DESTTERMZIP", required = false)
+    var destinationTerminalZip: String? = null,
+
+    @field:Element(name = "DESTTERMPHONE", required = false)
+    var destinationTerminalPhone: String? = null,
+
+    @field:Element(name = "TYPE", required = false)
+    var destinationTerminalType: String? = null
+)
+
 @Root(name = "ABF", strict = false)
 data class QuoteResponse(
+    //If there are errors
+    @field:Element(name = "NUMERRORS", required = false)
+    var numErrors: Int? = null,
+
+    @field:Element(name = "ERROR", required = false)
+    var error: Error? = null,
+
+    //If there are no errors
     @field:Element(name = "QUOTEID", required = false)
     var quoteID: String? = null,
 
@@ -26,9 +76,9 @@ data class QuoteResponse(
     @field:Element(name = "EXPIRATIONDATE", required = false)
     var expirationDate: String? = null,
 
-    @field:Element(name = "NUMERRORS", required = false)
-    var numErrors: Int? = null,
+    @field:Element(name = "ORIGTERMINFO", required = false)
+    var originInfo: OriginTerminalInfo? = null,
 
-    @field:Element(name = "ERROR", required = false)
-    var error: Error? = null
+    @field:Element(name = "DESTTERMINFO", required = false)
+    var destinationInfo: OriginTerminalInfo? = null
 )
