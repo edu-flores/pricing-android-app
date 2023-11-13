4fbf01122957e007e4861128d445e03ca46ebcd9
Carlos Lozada <ce.lozada@icloud.com>
2023-11-13 00:46:36 -0600

    Set on click listeners to update the values of each filed

--- app/src/main/java/com/example/pricingapp/CargoItem.kt
+++ app/src/main/java/com/example/pricingapp/CargoItem.kt
@@ -1,12 +1,17 @@
 package com.example.pricingapp
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +27,14 @@ class CargoItem : Fragment() {
 
     private var fragmentNumber = 0
 
+    lateinit var classValue : String
+    lateinit var weightValue : String
+    lateinit var packageValue : String
+    lateinit var quantityValue : String
+    lateinit var lengthValue : String
+    lateinit var widthValue : String
+    lateinit var heightValue : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (arguments != null) {
@@ -30,12 +43,113 @@ class CargoItem : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         val view = inflater.inflate(R.layout.fragment_cargo_item, container, false)
+
+        val itemNumberTextView = view.findViewById<TextView>(R.id.itemNumberTextView)
         val classSpinner = view.findViewById<Spinner>(R.id.itemClass)
         val packageSpinner = view.findViewById<Spinner>(R.id.itemType)
 
+        itemNumberTextView.text = "Item #$fragmentNumber"
         setupSpinner(classSpinner, R.array.classes)
         setupSpinner(packageSpinner, R.array.package_types)
+
+        val classValueField = view.findViewById<Spinner>(R.id.itemClass)
+        classValueField.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                classValue = parent.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing or handle 'nothing selected' state
+            }
+        }
+
+        val weightValueF = view.findViewById<EditText>(R.id.itemWeight)
+        weightValueF.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                weightValue = s.toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing or handle 'before text changed' state
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Do nothing or handle 'on text changed' state
+            }
+        })
+
+        val packageValueF = view.findViewById<Spinner>(R.id.itemType)
+        packageValueF.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                packageValue = parent.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing or handle 'nothing selected' state
+            }
+        }
+
+        val quantityValueF = view.findViewById<EditText>(R.id.itemQuantity)
+        quantityValueF.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                quantityValue = s.toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing or handle 'before text changed' state
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Do nothing or handle 'on text changed' state
+            }
+        })
+
+        val lengthValueF = view.findViewById<EditText>(R.id.itemLength)
+        lengthValueF.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                lengthValue = s.toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing or handle 'before text changed' state
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Do nothing or handle 'on text changed' state
+            }
+        })
+
+        val widthValueF = view.findViewById<EditText>(R.id.itemWidth)
+        widthValueF.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                widthValue = s.toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing or handle 'before text changed' state
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Do nothing or handle 'on text changed' state
+            }
+        })
+
+        val heightValueF = view.findViewById<EditText>(R.id.itemHeight)
+        heightValueF.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                heightValue = s.toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing or handle 'before text changed' state
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Do nothing or handle 'on text changed' state
+            }
+        })
 
         return view
     }
@@ -62,4 +176,5 @@ class CargoItem : Fragment() {
             spinner.adapter = adapter
         }
     }
+
 }
